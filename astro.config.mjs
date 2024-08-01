77ea8a45344fefc9fd2a4480a6afb9d469892538
shogo-t <shogo@chalkboard.me>
2024-08-02 00:02:01 +0900

    検索対応, vercelのアダプタを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,6 +17,7 @@ import { AdmonitionComponent } from "./src/plugins/rehype-component-admonition.m
 import { GithubCardComponent } from "./src/plugins/rehype-component-github-card.mjs"
 import {parseDirectiveNode} from "./src/plugins/remark-directive-rehype.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs"
+import vercel from '@astrojs/vercel/serverless';
 
 const oklchToHex = (str) => {
   const DEFAULT_HUE = 250
@@ -30,6 +31,8 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'hybrid',
+  adapter: vercel(),
   site: "https://fuwari.vercel.app/",
   base: "/",
   trailingSlash: "ignore",
---
+++ src/pages/api/search.json.ts
@@ -0,0 +1,28 @@
+export const prerender = false
+
+import { newtClient } from '@/lib/newt';
+import type { Article } from '@/lib/newt';
+
+export async function GET({request}) {
+    const url = new URL(request.url);
+    const params = new URLSearchParams(url.search);
+    const keyword = params.get('keyword');
+
+    const { items: allBlogPosts } = await newtClient.getContents<Article>({
+        appUid: 'blog',
+        modelUid: 'article',
+        query: {
+            body: {
+                match: keyword
+            }
+        }
+    });
+
+    console.log(allBlogPosts)
+
+    return new Response(JSON.stringify({ results: allBlogPosts }), {
+      headers: {
+          'Content-Type': 'application/json'
+      }
+  });
+}
